ca5b1a393c73b256b60bec3a1ec9c07ba5db99d8
sansu-07 <131161172+sansu-07@users.noreply.github.com>
2023-04-18 17:15:45 +0530

    Add files via upload

---
+++ lecture2-1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+ int a=10;
+ int b=20;
+ int c=30;
+ int d=a+b+c;
+ int e=c-b-a;
+ int f=a*b*c;
+ int g=c/a;
+ printf("d=%d\n",d);
+ printf("e=%d\n",e);
+ printf("f=%d\n",f);
+ printf("g=%d\n",g); 	
+}
