01788ab3926f759ef2e63f4ef83ac40987692ca6
sansu-07 <131161172+sansu-07@users.noreply.github.com>
2023-04-18 17:13:21 +0530

    Add files via upload

---
+++ task4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	printf("*****************\n");
+	printf("|\t\t|\n");
+	printf("|\t\t|\n");
+	printf("|\t\t|\n");
+	printf("|    SANSKAR    |\n");
+	printf("|\t\t|\n");
+	printf("|\t\t|\n");
+	printf("|\t\t|\n");
+	printf("*****************\n");
+}
