ee7d84567b0c7021162b12bd913a82cf2dc40089
sansu-07 <131161172+sansu-07@users.noreply.github.com>
2023-04-18 17:11:12 +0530

    Add files via upload

---
+++ l1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	printf("SANSKAR VIJAYBHAI BHANDERI\n");
+	printf("standard=11 science\n");
+	printf("age=15\n");
+	printf("school=PP SAVANI\n");
+	printf("address=4/satadhar soc,b\h saraswati school a.k.road,surat\n");
+	printf("father=vijaybhai nagjibhai bhanderi\n");
+	printf("mother=amishaben vijaybhai bhanderi\n");
+	printf("i am learning c languauge from red & white\n");
+	printf("i like to work on computer\n");
+	printf("i like to play cricket\n");
+	printf("uncle=anilbhai bhanderi\n");
+	printf("i am learning c language\n");
+}
