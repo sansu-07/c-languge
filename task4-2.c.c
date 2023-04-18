f331e42eee85b3b6045eca77ad0c9d4282d1595e
sansu-07 <131161172+sansu-07@users.noreply.github.com>
2023-04-18 17:13:49 +0530

    Add files via upload

---
+++ task4-2.c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	printf("-----------------\n");
+	printf("|\t\t|\n");
+	printf("S\t\t|\n");
+	printf("A\t\t|\n");
+	printf("N\t\t|\n");
+	printf("S\t\t|\n");
+	printf("K\t\t|\n");
+	printf("A\t\t|\n");
+	printf("R\t\t|\n");
+	printf("|\t\t|\n");
+	printf("-----------------\n");
+}
